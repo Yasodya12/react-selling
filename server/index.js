b60e6d4e11174092537bef82ac7585669d740a28
Yasodya12 <rishikayasodya12@gmail.com>
2023-12-26 16:40:28 +0530

    dtabase connected

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 import express from 'express'
+import mongoose from "mongoose";
 
 let app = express();
-
+mongoose.connect("mongodb+srv://root:1234@mern-estate.xic95pt.mongodb.net/mern-estateretryWrites=true&w=majority").then(()=>{
+    console.log("Connected");
+}).catch((err) =>{
+    console.log(err)
+})
 app.listen(3000, () =>{
     console.log("runing hellowsadsadasw");
 })
