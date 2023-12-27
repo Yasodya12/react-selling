1b147e4c08a465a6901b9f929fef75a28020e1e6
Yasodya12 <rishikayasodya12@gmail.com>
2023-12-27 20:21:39 +0530

    routes added

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import mongoose from "mongoose";
+import userRouter from "./routee/usersad.js"
 
 let app = express();
 mongoose.connect("mongodb+srv://root:1234@mern-estate.xic95pt.mongodb.net/mern-estateretryWrites=true&w=majority").then(()=>{
@@ -10,3 +11,8 @@ mongoose.connect("mongodb+srv://root:1234@mern-estate.xic95pt.mongodb.net/mern-e
 app.listen(3000, () =>{
     console.log("runing hellowsadsadasw");
 })
+
+app.get('/test', (req,res)=>{
+   res.send('hellow word');
+});
+app.use('/server/user',userRouter);
