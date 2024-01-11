21bd3c97b80e11062dcc18d81f99148bc69dd516
Yasodya12 <rishikayasodya12@gmail.com>
2024-01-11 10:46:42 +0530

    listing deleted added

--- client/src/view/pages/Profile.tsx
+++ client/src/view/pages/Profile.tsx
@@ -27,7 +27,7 @@ export default function Profile(){
     const [userListings, setUserListings] = useState([]);
     const dispatch = useDispatch();
     const navigate = useNavigate();
-
+    ;
     // useEffect(() => {
     //     if (file) {
     //         handleFileUpload(file);
@@ -121,6 +121,44 @@ export default function Profile(){
         navigate('/sign-in');
     };
 
+    const handleShowListings = async () => {
+        try {
+            setShowListingsError(false);
+            const res = await fetch(`http://localhost:4000/server/user/listings/${currentUser._id}`);
+            const data = await res.json();
+            if (data.success === false) {
+                setShowListingsError(true);
+                return;
+            }
+
+            setUserListings(data);
+            console.log(userListings);
+        } catch (error) {
+            setShowListingsError(true);
+        }
+    };
+
+
+    // @ts-ignore
+    const handleListingDelete = async (listingId) => {
+        try {
+            const res = await fetch(`http://localhost:4000/server/listing/delete/${listingId}`, {
+                method: 'DELETE',
+            });
+            const data = await res.json();
+            if (data.success === false) {
+                console.log(data.message);
+                return;
+            }
+
+            setUserListings((prev) =>
+                // @ts-ignore
+                prev.filter((listing) => listing._id !== listingId)
+            );
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     return (
             <div className='p-3 max-w-lg mx-auto'>
@@ -204,40 +242,68 @@ export default function Profile(){
                 <p id={"successful"} className='text-green-700 mt-5'>
                     {updateSuccess ? 'User is updated successfully!' : ''}
                 </p>
-
-                {/*</p>*/}
-                {/*<button  className='text-green-700 w-full'>*/}
-                {/*    Show Listings*/}
-                {/*</button>*/}
-                {/*<p className='text-red-700 mt-5'>*/}
-
-                {/*</p>*/}
-
-
-                {/*<div className='flex flex-col gap-4'>*/}
-                {/*    <h1 className='text-center mt-7 text-2xl font-semibold'>*/}
-                {/*        Your Listings*/}
-                {/*    </h1>*/}
-
-                {/*    <div*/}
-
-                {/*        className='border rounded-lg p-3 flex justify-between items-center gap-4'*/}
-                {/*    >*/}
+                <button onClick={handleShowListings} className='text-green-700 w-full'>
+                    Show Listings
+                </button>
+                <p className='text-red-700 mt-5'>
+                    {showListingsError ? 'Error showing listings' : ''}
+                </p>
 
 
 
-                {/*        <div className='flex flex-col item-center'>*/}
-                {/*            <button*/}
+                {userListings && userListings.length > 0 && (
+                    <div className='flex flex-col gap-4'>
+                        <h1 className='text-center mt-7 text-2xl font-semibold'>
+                            Your Listings
+                        </h1>
+                        {userListings.map((listing) => (
+                            <div
+                                // @ts-ignore
+                                key={listing._id}
+                                className='border rounded-lg p-3 flex justify-between items-center gap-4'
+                            >
 
-                {/*                className='text-red-700 uppercase'*/}
-                {/*            >*/}
-                {/*                Delete*/}
-                {/*            </button>*/}
+                                <Link
+                                    // @ts-ignore
+                                    to={`/listing/${listing._id}`}>
+                                    <img
+                                        // @ts-ignore
+                                        src={listing.imageUrls[0]}
+                                        alt='listing cover'
+                                        className='h-16 w-16 object-contain'
+                                    />
+                                </Link>
+                                <Link
+                                    className='text-slate-700 font-semibold  hover:underline truncate flex-1'
+                                    // @ts-ignore
+                                    to={`/listing/${listing._id}`}
+                                >
 
-                {/*        </div>*/}
-                {/*    </div>*/}
+                                    <p>
 
-                {/*</div>*/}
+                                        { // @ts-ignore
+                                            listing.name}</p>
+                                </Link>
+
+                                <div className='flex flex-col item-center'>
+                                    <button
+                                        // @ts-ignore
+                                        onClick={() => handleListingDelete(listing._id)}
+                                        className='text-red-700 uppercase'
+                                    >
+                                        Delete
+                                    </button>
+
+                                    <Link
+                                        // @ts-ignore
+                                        to={`/update-listing/${listing._id}`}>
+                                        <button className='text-green-700 uppercase'>Edit</button>
+                                    </Link>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )}
 
             </div>
         );
